ac4736f6ce87b13587cb5075b0da97b51f9f969c
Jacob Zhang <jacob-z@live.de>
2025-12-24 14:52:10 +0100

    feat: add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import '../app.css';
 	import '$lib/i18n'; // Initialize i18n
-	import favicon from '$lib/assets/favicon.svg';
 	import Toast from '$lib/components/ui/primitives/Toast.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
-	<title>Liszt'n Up!</title>
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="Liszt’n Up!" />
+	<link rel="manifest" href="/site.webmanifest" />
+	<title>Liszt’n Up!</title>
 </svelte:head>
 
 <div class="min-h-full w-full">
