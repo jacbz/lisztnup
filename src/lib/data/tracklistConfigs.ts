8e18585f552cb28e254e8a728f8216bc2de0e2f3
Jacob Zhang <jacob-z@live.de>
2026-03-04 00:55:07 +0100

    feat: expand concerto playlists

--- src/lib/data/tracklistConfigs.ts
+++ src/lib/data/tracklistConfigs.ts
@@ -212,11 +212,34 @@ export const PIANOCONCERTO_CONFIG: TracklistConfig = {
 		solo: -MAX_WORK_SCORE_ROUNDED
 	},
 	limitWorksFromComposer: 0.1,
-	nameFilter: ['piano', 'klavier'],
+	nameFilter: ['piano', 'klavier', 'keyboard'],
 	includeWorks: [
-		'487b34d5', // Boulanger - Fantaisie variée
-		'60b22df4', // Gershwin - Rhapsody in Blue
-		'21560c8d' // Schumann - Konzertstück
+		'155c936d', // Albéniz – Concierto fantástico, op. 78
+		'456f270a', // Alkan – 2 concerti da camera, op. 10
+		'487b34d5', // Boulanger – Fantaisie variée
+		'76d9aed3', // Chopin – Variations in B‐flat major on “Là ci darem la mano”, op. 2
+		'907d0a19', // Dohnányi – Variations on a Nursery Song, op. 25
+		'60b22df4', // Gershwin – Rhapsody in Blue (orch. Grofé)
+		'2f6a2739', // Gubaidulina – Introitus
+		'0f84285d', // Hindemith – Kammermusik No. 2, op. 36 no. 1
+		'8b5584aa', // Liszt – Malédiction, S. 121
+		'32eb27c7', // Liszt – Totentanz, S. 126/2
+		'ca139438', // Litolff – Concerto Symphonique no. 4 in D minor, op. 102
+		'550b7c98', // McPhee – Tabuh-Tabuhan
+		'2c09a562', // Messiaen – Awakening of the Birds
+		'c9299e16', // Messiaen – Couleurs de la cité céleste, I/46
+		'5028903b', // Messiaen – Oiseaux exotiques, I/41
+		'244c5897', // Montsalvatge – Concerto breve
+		'67b7d3b4', // Pärt – Lamentate
+		'e04dfc83', // Poulenc – Aubade, FP 51
+		'17eea09b', // Rachmaninoff – Rhapsody on a Theme of Paganini, op. 43
+		'9108faed', // Respighi – Concerto in modo misolidio, P. 145
+		'11f16f24', // Saint‐Saëns – Allegro appassionato in C-sharp minor, op. 70
+		'21560c8d', // Schumann – Konzertstück (Introduction and Allegro appassionato) in G major, op. 92
+		'c77ae8b7', // Sylvestrov – Two Dialogues with Postscript
+		'b908a5cf', // Tchaikovsky – Concert Fantasia, op. 56
+		'23d2258b', // Turina – Rapsodia sinfónica, op. 66
+		'578ccd3a' // Weber – Konzertstück in F minor, op. 79
 	],
 	composerFilter: {
 		mode: 'exclude',
@@ -237,11 +260,54 @@ export const VIOLINCONCERTO_CONFIG: TracklistConfig = {
 		solo: -MAX_WORK_SCORE_ROUNDED
 	},
 	limitWorksFromComposer: 0.1,
-	nameFilter: ['/viol(i|o)n\\b/'],
+	nameFilter: ['/viol(i|o)n(e|o|i|s)?\\b/'],
 	includeWorks: [
-		'22393fc2', // Bernstein - Serenade after Plato's Symposium
-		'561b8302', // Vaughan Williams - The Lark Ascending
-		'24c323a3' // Ysaÿe - Amitié
+		'4cbf3d3e', // Albinoni – 12 Concerti a cinque, op. 10
+		'e858c951', // Berlioz – Rêverie et caprice, op. 8, H. 88
+		'22393fc2', // Bernstein – Serenade after Plato’s “Symposium”
+		'c1882b78', // Bruch – Adagio appassionato, op. 57
+		'e749559a', // Dutilleux – L'arbre des songes
+		'54b5e409', // Dvořák – Romance in F minor, op. 11, B. 39
+		'a69778c2', // Glass – Echorus
+		'18a32371', // Glazunov – Mazurka-oberek in D major
+		'12c9bedf', // Gubaidulina – In tempus praesens
+		'b56e0a48', // Hartmann – Concerto funebre
+		'4fe606f0', // Holst – A Song of the Night, op. 19 no. 1
+		'6f43bf85', // Janáček – The Wandering of a Little Soul
+		'9cf137ed', // Lalo – Symphonie espagnole in D minor, op. 21
+		'4790c2dd', // Pärt – Darf ich…
+		'914919ea', // Pärt – Tabula rasa
+		'd6e59d93', // Ravel – Tzigane
+		'4b80028e', // Saint‐Saëns – Caprice andalous en sol majeur, op. 122
+		'40c9ecdf', // Saint‐Saëns – Morceau de concert, op. 62
+		'e0818f8b', // Taneyev – Suite de concert, op. 28
+		'c3912d1e', // Tchaikovsky – Sérénade mélancolique in B-flat minor, op. 26
+		'862a0871', // Tchaikovsky – Souvenir d’un lieu cher, op. 42 (orch. Glazunov)
+		'128dfe93', // Vasks – Distant Light
+		'2b2ee1a6', // Vasks – Lonely Angel
+		'561b8302', // Vaughan Williams – The Lark Ascending
+		'9e283dd5', // Vivaldi – 6 concerti, op. 11
+		'c487e7e1', // Vivaldi – 6 concerti, op. 12
+		'de81e7e5', // Vivaldi – 6 concerti, op. 6
+		'9c946ce9', // Vivaldi – Concerto in A, RV 552 "Per eco in lontano"
+		'4045e2a7', // Vivaldi – Concerto in B-flat major, RV 553
+		'df225ba0', // Vivaldi – Concerto in C minor, RV 199 "Il sospetto"
+		'6f4a5507', // Vivaldi – Concerto in D major, RV 211
+		'86298a29', // Vivaldi – Concerto in D minor, RV 243
+		'96341344', // Vivaldi – Concerto in D, RV 562
+		'f16cfe09', // Vivaldi – Concerto in do maggiore Per la SS. Assunzione di Maria Vergine, RV 581
+		'b79443b8', // Vivaldi – Concerto in E major, RV 270
+		'd9aee017', // Vivaldi – Concerto in E-flat major, RV 257
+		'598ed597', // Vivaldi – Concerto in F major, RV 286
+		'161dd98a', // Vivaldi – L’estro armonico, op. 3
+		'fe884180', // Vivaldi – La cetra, op. 9
+		'bf690cb7', // Vivaldi – La stravaganza, op. 4
+		'7044d543', // Vivaldi – The Contest Between Harmony and Invention, op. 8
+		'87886dcf', // Vivaldi – The Four Seasons
+		'2d3c2e29', // Wieniawski – Capriccio-Valse, op. 7
+		'2154dc72', // Wieniawski – Polonaise Brillante en la majeur, op. 21
+		'24c323a3', // Ysaÿe – Amitié, op. 26
+		'a36e54e5' // Ysaÿe – Poème élégiaque, op. 12
 	]
 };
 
@@ -258,10 +324,19 @@ export const CELLOCONCERTO_CONFIG: TracklistConfig = {
 		solo: -MAX_WORK_SCORE_ROUNDED
 	},
 	limitWorksFromComposer: 0.1,
-	nameFilter: ['cello'],
+	nameFilter: ['violoncello', '/\\bcello/'],
 	includeWorks: [
-		'0e9d27ec', // Bloch - Schemolo
-		'514024f4', // Bruch - Kol Nidrei
+		'0e9d27ec', // Bloch – Schelomo
+		'514024f4', // Bruch – Kol Nidrei, op. 47
+		'1800c589', // Dun – The Eternal Vow
+		'c472459c', // Dutilleux – Tout un monde lointain...
+		'99c6d084', // Hindemith – Kammermusik No. 3, op. 36 no. 2
+		'65f6580f', // Leo – Concerto no. 1 in A major
+		'df1f9625', // Leo – Concerto no. 3 in D minor
+		'ddd19c33', // Leo – Concerto no. 4 in A major
+		'370dd15d', // Leo – Concerto no. 5 in F minor
+		'8709d5f7', // Leo – Sinfonia concertata (Concerto no. 6) in C minor
+		'890399dc', // Offenbach – Concerto militaire
 		'942499a7' // Tchaikovsky - Variations on a Rococo Theme
 	]
 };
